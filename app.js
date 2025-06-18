50730989320bfb197d75c38f71fa09cd47addb51
manzaero <ligoero@gmail.com>
2025-06-18 21:50:08 +0300

    refact mongo url to .env

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path')
 
 const authenticated = require('./middlewares/authenticated');
 
 const port = 3001;
 const app = express();
+
+
+
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -15,14 +19,20 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
+app.use(express.static(path.resolve('..', 'front', 'dist')))
+
 app.use('/api', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
 app.use('/api/categories', require('./routes/categories'));
 
 app.use('/api/cart', authenticated, require('./routes/cart'));
 
+app.get("/*path", (req, res) => {
+    res.sendFile(path.resolve("..", "front", "dist", "index.html"))
+});
+
 mongoose
-    .connect('mongodb+srv://overseer:o5393687@notes.cj1p78t.mongodb.net/shop?retryWrites=true&w=majority')
+    .connect(process.env.MONGO_URL)
     .then(() => {
         app.listen(port, () => {
             console.log(`Server started on port ${port}`);
